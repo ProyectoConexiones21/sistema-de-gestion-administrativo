0b42be35bf5e8c71b233dade2583069a6acc3220
ProyectoConexiones21 <124088961+ProyectoConexiones21@users.noreply.github.com>
2023-09-30 10:10:43 -0400

    Moises 30/9/2023 10:10am

--- modelo/administrador.sql
+++ modelo/administrador.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 20-09-2023 a las 22:06:37
+-- Tiempo de generación: 30-09-2023 a las 16:09:32
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
--- modelo/inicio.php
+++ modelo/inicio.php
@@ -9,7 +9,7 @@ $user= new inicio();
 if(isset($_SESSION['user'])){
 /* echo "Hay sesion"; */
 $user->setUser($userSession->getActiUser());
-    include_once('vista/menu.php');
+    $pagina="menu";
 }
 else if(isset($_POST['usuario']) && isset($_POST['contraseña'])){
     /* echo "validacion de sesion"; */
@@ -18,19 +18,19 @@ else if(isset($_POST['usuario']) && isset($_POST['contraseña'])){
     $passForm= $_POST['contraseña'];
 
     if($user->usuarioExiste($userForm, $passForm)){
-        /* echo "usuario validado"; */
+       /*  echo "usuario validado"; */
         $userSession->setActiUser($userForm);
         $user->setUser($userForm);
-        include_once("vista/menu.php");
+        $pagina="menu";
     }
     else{
         /* echo "usuario sin validar"; */
         $errorLogin = "Nombre de usuario y/o password incorrecto";
-        include_once("vista/inicio.php");
+        $pagina="inicio";
     }
 }
 else{
     /* echo "login"; */
-    include_once("vista/inicio.php");
+    $pagina="inicio";
 }
 ?>
